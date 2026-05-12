00cb8645030dd7e8ad22dbc50a613459cc698c91
chandraathithyaM <chandraathithyam@gmail.com>
2026-05-12 09:38:28 +0530

    Add new allowed origin for CORS configuration

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://payment-gateway-js.vercel.app',
   'https://payment-gateway-js-git-main-chandraathithyams-projects.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
